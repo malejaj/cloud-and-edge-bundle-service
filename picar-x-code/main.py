d09f459ddf774e8391ac0058639d37a9ac4e0a6f
malejaj <majimenezh@unal.edu.com>
2025-09-04 17:00:13 +0200

    take times

--- picar-x-code/main.py
+++ picar-x-code/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 import webbrowser
 import json
 
-px = Picarx()
+px = None
+
 
 
 current_state = None
@@ -26,7 +27,7 @@ responses_cloud = []
 
 bundleIP="140.93.97.159"
 port = 8000
-
+iter =40
 
 #latency save
 def save_response_times_to_file(filename='cloud_edge_response_latency.json'):
@@ -104,24 +105,25 @@ def decision(api, endpoint):
     global px_power
     i = 0
     response_data = []
-    while True:
+    #for i in range(iter):
     #while i < 100:
-        ultrasonic_percept = px.ultrasonic.read()
-        data = {
-            "front":ultrasonic_percept,
-            "vitesse":px_power
-            }
-        t1 = time.time()
-        url = f'http://{bundleIP}:{8000}/{api}/{endpoint}'
-        response = requests.post(url=url, json=data)
-        t2 = time.time()
-        response_data.append((t2 - t1) * 1000)
-        print(response.json())
-        print('latency detection [edge] = ', (t2 - t1) * 1000)
-        with lock:
-            px_power = response.json()
-        #i = i + 1
-        responses.append(response_data)
+    ultrasonic_percept = px.ultrasonic.read()
+    data = {
+        "front":ultrasonic_percept,
+        "vitesse":px_power
+        }
+    t1 = time.time()
+    url = f'http://{bundleIP}:{8000}/{api}/{endpoint}'
+    response = requests.post(url=url, json=data)
+    t2 = time.time()
+    response_data.append((t2 - t1) * 1000)
+    #print(response.json())
+    print('latency detection [edge] = ', (t2 - t1) * 1000)
+    with lock:
+        px_power = response.json()
+        print("vitesse", px_power)
+    #i = i + 1
+    responses.append(response_data)
 def stop():
     px.stop()
     
@@ -147,46 +149,65 @@ def identification(api,endpoint):
     i = 0
     response_data = []
     #while True:
-    while i < 100:
-        ultrasonic_percept = px.ultrasonic.read()
-        data = {
-            "distance":ultrasonic_percept
-        }
-        print("identification" , data)
-        t1 = time.time()
-        url =f'http://{bundleIP}:{8000}/{api}/{endpoint}'
-        response = requests.post(url=url, json=data)
-        print(response.json())
-        if response.status_code == 200 and response.json():
-            response = response.json()
-            #print(response.json())
-            #classId = response["classId"]
-            t2 = time.time()
-            print('delay identification [cloud] = ', (t2 - t1) * 1000)
-            response_data.append((t2 - t1) * 1000)
-        i = i + 1
+    #while i < 100:
+    ultrasonic_percept = px.ultrasonic.read()
+    data = {
+        "distance":ultrasonic_percept
+    }
+    #print("identification" , data)
+    t1 = time.time()
+    url =f'http://{bundleIP}:{8000}/{api}/{endpoint}'
+    response = requests.post(url=url, json=data)
+    #print(response.json())
+    if response.status_code == 200 and response.json():
+        response = response.json()
+        #print(response.json())
+        #classId = response["classId"]
+        t2 = time.time()
+        print('delay identification [cloud] = ', (t2 - t1) * 1000)
+        response_data.append((t2 - t1) * 1000)
+        #i = i + 1
+    responses_cloud.append(response_data)
+
+def get_px():
+    global px
+    if px is None:
+        px = Picarx()
+    return px
+
 
 if __name__=='__main__':
+   
 
+    px = get_px()
     reponse = requests.get(url=f"http://{bundleIP}:{port}/get-bundle")
     data = reponse.json()
     t1 = time.time()
     trajectory_planning(data['api'], data['endpoint3'])
     t2 = time.time()
     print('delay trajectory [cloud] = ', (t2 - t1) * 1000)
-    i = 0
-    while i < 2:       
-        thread1 = threading.Thread(target=circulation)
+ 
+    
+    for i in range(10):
+        #thread1 = threading.Thread(target=circulation)
         thread2 = threading.Thread(target=decision, args=(data['api'], data['endpoint1'],))
-        thread3 = threading.Thread(target=identification, args=(data['api'], data['endpoint4'], ))
+        thread3 = threading.Thread(target=identification, args=(data['api'], data['endpoint4'],))
 
-        thread1.start()
+        #thread1.start()
         thread2.start()
         thread3.start()
-        #uvicorn.run("main:app",host="0.0.0.0",port = 4000,reload=True)
-        thread1.join()
+
+        #thread1.join()
         thread2.join()
         thread3.join()
-        i += 1
-#save_response_times_to_file()
-#save_response_times2_to_file()
+
+    save_response_times_to_file()
+    save_response_times2_to_file()
+    '''
+      decision(data['api'], data['endpoint1'])
+        save_response_times_to_file()
+    print("hola")
+    for i in range(10):
+        decision(data['api'], data['endpoint5'])
+        save_response_times_to_file()
+        '''
